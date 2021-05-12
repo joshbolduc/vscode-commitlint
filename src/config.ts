2a6284a3f1d6326e52d5cb45e577d232ad842ded
yi_Xu <xjy123456120@163.com>
2021-05-12 07:44:46 -0500

    fix: support config file as absolute path (#89)

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,34 @@
+import { dirname, isAbsolute, relative } from 'path';
 import load from '@commitlint/load';
 import { LoadOptions } from '@commitlint/types';
 import { workspace } from 'vscode';
+import { getLogger } from './log';
 import { getConfigFile, getExtendConfiguration } from './settings';
 
 export async function loadConfig(path: string | undefined) {
-  const configFile = getConfigFile();
+  const configOverwriteFile = getConfigFile() || '';
   const extendsRules = getExtendConfiguration('rules');
-
-  const loadOptions: LoadOptions = configFile
+  const workspacePath = workspace.workspaceFolders?.[0].uri.fsPath;
+  const configOptions = isAbsolute(configOverwriteFile)
     ? {
-        cwd: workspace.workspaceFolders?.[0].uri.fsPath,
-        file: configFile,
+        cwd: dirname(configOverwriteFile),
+        file: relative(dirname(configOverwriteFile), configOverwriteFile),
       }
+    : {
+        cwd: workspacePath,
+        file: configOverwriteFile,
+      };
+
+  const loadOptions: LoadOptions = configOverwriteFile
+    ? configOptions
     : { cwd: path };
 
   const config = await load({}, loadOptions);
+  getLogger().appendLine(
+    `[${new Date().toLocaleString()}] loadOptions: ${JSON.stringify(
+      loadOptions,
+    )}`,
+  );
 
   return { ...config, rules: { ...extendsRules, ...config.rules } };
 }
