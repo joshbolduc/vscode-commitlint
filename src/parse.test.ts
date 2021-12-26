b35372848a52b35ccc5ec43da06c93d3e7d3c8f2
Josh Bolduc <joshbolduc@users.noreply.github.com>
2021-12-26 16:08:52 -0500

    test: add v14-v16 tests

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readdirSync, readFileSync, statSync } from 'fs';
 import { join, resolve } from 'path';
 import { fixturesPath, testLibRootPath } from '../test/util';
 import { parseCommit } from './parse';
@@ -7,23 +7,77 @@ jest.mock('./log');
 jest.mock('./settings');
 
 describe('parse', () => {
-  const versions = [11, 12, 13];
+  const versions = readdirSync(testLibRootPath).filter((item) =>
+    statSync(resolve(testLibRootPath, item)).isDirectory(),
+  );
 
   const fixtures = [
-    'commit-all-ranges.txt',
-    'commit-leading-comment.txt',
-    'commit-leading-comment-newlines.txt',
-    'commit-trailing-comment.txt',
-  ];
+    [
+      'commit-all-ranges.txt',
+      {
+        ranges: {
+          body: [79, 171],
+          footer: [173, 184],
+          header: [25, 52],
+          scope: [30, 35],
+          type: [25, 29],
+        },
+        sanitizedText: `feat(scope): commit subject
+
+
+Commit body text here.
+Commit body text here.
+Commit body text here.
+
+Closes #123`,
+      },
+    ],
+    [
+      'commit-leading-comment.txt',
+      {
+        ranges: {
+          body: [18, 27],
+          header: [10, 16],
+        },
+        sanitizedText: `Header
+
+Body line`,
+      },
+    ],
+    [
+      'commit-leading-comment-newlines.txt',
+      {
+        ranges: {
+          body: [20, 29],
+          header: [12, 18],
+        },
+        sanitizedText: `Header
+
+Body line`,
+      },
+    ],
+    [
+      'commit-trailing-comment.txt',
+      {
+        ranges: {
+          body: [8, 17],
+          header: [0, 6],
+        },
+        sanitizedText: `Header
+
+Body line`,
+      },
+    ],
+  ] as const;
 
   versions.forEach((version) => {
-    const libPath = resolve(testLibRootPath, `v${version}`);
+    const libPath = resolve(testLibRootPath, `${version}`);
 
-    fixtures.forEach((fixture) => {
-      it(`parses ${fixture} using v${version}`, async () => {
+    fixtures.forEach(([fixture, expected]) => {
+      it(`parses ${fixture} using ${version}`, async () => {
         const contents = readFileSync(join(fixturesPath, fixture)).toString();
 
-        expect(await parseCommit(contents, libPath)).toMatchSnapshot();
+        expect(await parseCommit(contents, libPath)).toStrictEqual(expected);
       });
     });
   });
