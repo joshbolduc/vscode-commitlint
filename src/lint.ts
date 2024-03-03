454a247a9c844e4723ea1dad6ffe184d6f49fce3
Josh Bolduc <joshbolduc@users.noreply.github.com>
2024-03-03 16:34:47 -0500

    fix: use dynamic import instead of `require` to support commitlint v19
    Fixes #753

--- src/lint.ts
+++ src/lint.ts
@@ -38,7 +38,7 @@ export async function runLint(text: string, path: string | undefined) {
   );
   updateStatusBar(ruleCount);
 
-  const lint = importCommitlintLint(path);
+  const lint = await importCommitlintLint(path);
 
   return {
     problems: await lint(text, config.rules, {
